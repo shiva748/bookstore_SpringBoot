f1419469cd2babbac1e71e72848bd02b5b80e3cd
Shiva <Shivagautam2002@gmail.com>
2025-09-19 09:21:52 +0530

    Working with Spring Security

--- src/main/java/com/shiva/bookstore/config/SecurityConfig.java
+++ src/main/java/com/shiva/bookstore/config/SecurityConfig.java
@@ -2,20 +2,20 @@ package com.shiva.bookstore.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                )
-                .csrf(csrf -> csrf.disable()); // Disables CSRF protection
-
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/auth/**").permitAll()).authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated()).formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 }
--- src/main/java/com/shiva/bookstore/controller/RootController.java
+++ src/main/java/com/shiva/bookstore/controller/RootController.java
@@ -1,16 +1,23 @@
 package com.shiva.bookstore.controller;
 
 import com.shiva.bookstore.dto.SimpleResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
     @GetMapping
-    public ResponseEntity<?> root() {
-        return ResponseEntity.status(HttpStatus.OK).body(new SimpleResponse(true, "Hello there, Our Api is live."));
+    public ResponseEntity<?> root(HttpServletRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(new SimpleResponse(true, "Hello there, Our Api is live and your session id is "+request.getSession().getId()));
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken getCsrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
     }
     
 }
